b8a5dd2d6cfe1f6432da34fcc663d9a49d87e3e1
DemaPy <benderdema@gmail.com>
2026-03-29 16:34:47 +0200

    refactor: move error handling to main function by utilizing Result enum

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use ::std::{env, fs};
+use ::std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     let contents =
         fs::read_to_string(config.file_path).expect("Something went wrong reading the file");
 
-    println!("With text:\n{}, {}", contents, config.query);
+    println!("With text:\n{}", contents);
 }
 
 struct Config {
@@ -17,14 +20,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("Not enough arguments");
+            return Err("Not enough arguments");
         }
 
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
