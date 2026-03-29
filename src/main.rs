dee37c0a0496472c91fe38880813cd07dc0f6015
DemaPy <benderdema@gmail.com>
2026-03-29 16:37:00 +0200

    refactor: extract logic from main to run function

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ use ::std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
+    run(config);
+}
+
+fn run(config: Config) {
     let contents =
         fs::read_to_string(config.file_path).expect("Something went wrong reading the file");
 
