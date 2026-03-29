e58fd6ff061410af908adf21f98ceb82726ffcec
DemaPy <benderdema@gmail.com>
2026-03-29 15:05:48 +0200

    feat: read file by file_path introduced in args

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use::std::env;
+use::std:: {
+    env,
+    fs
+};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,4 +10,8 @@ fn main() {
     let file_path = &args[2];
 
     println!("Searching for {} in {}", query, file_path);
+
+    let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
