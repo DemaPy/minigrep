323083aa5a103bafddc72acec098db2bed135bb5
DemaPy <benderdema@gmail.com>
2026-03-29 17:03:44 +0200

    feat: introduce search function in lib crate; use search fn in run fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use minigrep::search;
+
 use std::error::Error;
 use std::{env, fs, process};
 
@@ -17,7 +19,9 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{line}")
+    }
 
     Ok(())
 }
