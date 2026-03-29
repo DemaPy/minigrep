0a6f474c9c0088628561a6ddd40347688a7383ec
DemaPy <benderdema@gmail.com>
2026-03-29 18:38:59 +0200

    feat: extend Config with ignore_case bool flag

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use minigrep::search;
+use minigrep::{search, search_case_insensitive};
 
 use std::error::Error;
 use std::{env, fs, process};
@@ -19,7 +19,13 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let lines = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in lines {
         println!("{line}")
     }
 
@@ -29,6 +35,7 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 struct Config {
     query: String,
     file_path: String,
+    ignore_case: bool,
 }
 
 impl Config {
@@ -40,6 +47,12 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
