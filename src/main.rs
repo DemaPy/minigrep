3ee2e1b8b420e29e3e0b87c844984a4ff1282412
DemaPy <benderdema@gmail.com>
2026-03-29 16:02:56 +0200

    refactor: implement Constructor for Config struct

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
-use::std:: {
-    env,
-    fs
-};
+use ::std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, file_path) = parse_config(&args);
+    let config = Config::new(&args);
 
-    let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Something went wrong reading the file");
 
-    println!("With text:\n{}", contents);
+    println!("With text:\n{}, {}", contents, config.query);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    if args.len() < 3 {
-        panic!("Not enough arguments");
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("Not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
     }
-
-    let query = &args[1];
-    let file_path = &args[2];
-
-    ( query, file_path )
 }
